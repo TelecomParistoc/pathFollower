6649a2db54cfd77483f01bb38b6f56bb1ec6cdf6
Telecom Robotics <telecom.robotics@gmail.com>
2016-01-30 18:43:36 +0100

    little by little

--- Corrections/pathFollower.hpp
+++ Corrections/pathFollower.hpp
@@ -27,7 +27,7 @@ class PathFollower
         static std::pair<float,float> getAngleDistance(float x1, float y1, float x2, float y2);
 
         static void standardCallback();
-        void rotateCallback(struct motionElement* element);
+        static void rotateCallback(struct motionElement* element);
         static void endCallback(struct motionElement* element);
 
     private:
