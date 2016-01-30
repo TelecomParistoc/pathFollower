2528f91da54e151de6bb9e6f4efc8bdccd2a3154
Telecom Robotics <telecom.robotics@gmail.com>
2016-01-30 19:15:08 +0100

    little by little

--- Corrections/main.cpp
+++ Corrections/main.cpp
@@ -9,8 +9,6 @@ int main()
     setRobotDistance(0);
 
     std::vector<float> pointsToVisit;
-    pointsToVisit.push_back(200);
-    pointsToVisit.push_back(900);
     pointsToVisit.push_back(700);
     pointsToVisit.push_back(50);
     pointsToVisit.push_back(1200);
