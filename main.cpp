6a1600decb00058e093bab29fd29df0bc3950804
superpingu <arnaudbonetti@me.com>
2016-02-08 22:22:39 +0100

    re-adding new file from Corrections

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include "pathFollower.hpp"
+
+
+int main()
+{
+    initMotionController();
+    setRobotDistance(0);
+
+    std::vector<float> pointsToVisit;
+    pointsToVisit.push_back(700);
+    pointsToVisit.push_back(70);
+    pointsToVisit.push_back(1200);
+    pointsToVisit.push_back(900);
+    pointsToVisit.push_back(200);
+    pointsToVisit.push_back(200);
+    pointsToVisit.push_back(900);
+    pointsToVisit.push_back(1850);
+
+    PathFollower::setCurrentPositionDirection(200,900,1,0);
+    PathFollower::followPath(pointsToVisit);
+
+    while(1);
+
+    return 0;
+}
---
+++ pathFollower.hpp
@@ -0,0 +1,41 @@
+#ifndef PATHFOLLOWER_HPP
+#define PATHFOLLOWER_HPP
+
+
+#include <functional>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <list>
+
+#include "robotdriver/headingcontroller.h"
+#include "robotdriver/motioncontroller.h"
+#include "robotdriver/speedcontroller.h"
+#include "robotdriver/motordriver.h"
+
+
+#ifndef M_PI
+#define M_PI 3.14159265
+#endif
+
+
+class PathFollower
+{
+    public:
+        static void setCurrentPositionDirection(float x, float y, float dirX, float dirY);
+        static void followPath(const std::vector<float>& path);
+
+        static std::pair<float,float> getAngleDistance(float x1, float y1, float x2, float y2);
+
+        static void standardCallback();
+        static void rotateCallback(struct motionElement* element);
+
+    private:
+        static float curPosX, curPosY;
+        static float curAngle;
+
+        static std::list<float> angles, distances;
+};
+
+
+#endif
